2fd66b616cfbf70223ecbdf2e15982edc5a9521b
victor loginov <tkoe88@zoho.com>
2024-10-22 22:13:40 +0200

    updated navlink

--- src/components/navigation/NavButton.tsx
+++ src/components/navigation/NavButton.tsx
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NavLink, useLocation } from 'react-router-dom';
+import { isMobile } from 'react-device-detect';
 import css from './NavButton.module.css';
 
 const icons = {
@@ -17,12 +18,10 @@ const labels = {
 export default function NavButton({ path }: { path: string }) {
   const { pathname } = useLocation();
   const isActive = pathname === path;
+  const classes = `${css['nav-link']} ${isActive ? css['active'] : ''} ${isMobile ? css['mobile'] : ''}`;
 
   return (
-    <NavLink
-      className={`${css['nav-link']} ${isActive ? css['active'] : ''}`}
-      to={path}
-    >
+    <NavLink className={classes} to={path}>
       <FontAwesomeIcon icon={icons[path as keyof IconProp]} size='2x' />
       {labels[path as keyof typeof labels]}
       {isActive && <motion.div layoutId='tab-indicator' className={css['active-tab']} />}
