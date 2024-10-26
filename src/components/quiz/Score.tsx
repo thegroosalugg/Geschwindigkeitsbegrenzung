0ff8673643ebb8f27c0d8dc2b626167af6d9457a
victor loginov <tkoe88@zoho.com>
2024-10-26 18:17:34 +0200

    added display item component.

--- src/components/quiz/Score.tsx
+++ src/components/quiz/Score.tsx
@@ -3,27 +3,53 @@ import User from '@/model/User';
 import Timer from '@/model/Timer';
 import css from './Score.module.css';
 
+interface DispItemProps {
+           item: number;
+  shouldAnimate: boolean;
+       isTotal?: boolean;
+}
+
+const DisplayItem = ({ item, shouldAnimate, isTotal }: DispItemProps) => {
+  return (
+    <motion.p
+      initial={false}
+      animate={{
+             scale: shouldAnimate && !isTotal ? [1, 1.2, 1] : 1,
+        transition: { ease: 'easeInOut', duration: 0.5, delay: 0.1 },
+      }}
+    >
+      <motion.span
+        initial={false}
+        animate={{
+             opacity: shouldAnimate ? [0, 1] : 1,
+          transition: { ease: 'easeInOut', duration: 1.5, },
+        }}
+      >
+        {item}
+      </motion.span>
+    </motion.p>
+  );
+};
+
 interface ScoreProps {
-  user: User;
+   user: User;
   timer: Timer;
 }
 
 export default function Score({ user, timer }: ScoreProps) {
-  const { solved, missed, score, total } = user;
+  const { isCorrect, solved, missed, score, total } = user;
+  const onIsRight =  isCorrect && timer.isStopped;
+  const onIsWrong = !isCorrect && timer.isStopped;
 
   return (
     <div className={css['score']}>
-      <motion.p>
-        <motion.span>{solved}</motion.span>
-      </motion.p>
-      <motion.p>
-        <motion.span>{total}</motion.span>
-      </motion.p>
-      <motion.p>
-        <motion.span>{missed}</motion.span>
-      </motion.p>
+      <DisplayItem item={solved} shouldAnimate={onIsRight} />
+      <DisplayItem item={total}  shouldAnimate={onIsRight} isTotal />
+      <DisplayItem item={missed} shouldAnimate={onIsWrong} />
       <AnimatePresence>
-        {score > 0 && timer.isAnimating && <motion.div className={css['pop-up']}>{score}</motion.div>}
+        {onIsRight && timer.isAnimating && (
+          <motion.div className={css['pop-up']}>{score}</motion.div>
+        )}
       </AnimatePresence>
     </div>
   );
