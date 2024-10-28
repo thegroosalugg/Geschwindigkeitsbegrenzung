9d3a8d86391a8046485330cb41370b8194ba8d4f
victor loginov <tkoe88@zoho.com>
2024-10-28 20:35:09 +0100

    replaced navlink with button. added debouncing

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,11 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import NavButton from './NavButton';
 import css from './NavBar.module.css';
 
 export default function NavBar() {
+  const navigate = useNavigate();
+  const [isDebouncing, setIsDebouncing] = useState(false);
+
+  function navTo(path: string) {
+    if (!isDebouncing) {
+      setIsDebouncing(true);
+      navigate(path);
+
+      setTimeout(() => {
+        setIsDebouncing(false);
+      }, 1000);
+    }
+  }
+
   return (
     <nav className={css['nav']}>
-      <NavButton path='/' />
-      <NavButton path='/settings' />
+      <NavButton path='/'         navFn={navTo} isDebouncing={isDebouncing} />
+      <NavButton path='/settings' navFn={navTo} isDebouncing={isDebouncing} />
     </nav>
   );
 }
