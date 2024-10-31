2f287f12100fd8f4e5a3319dcd8badb5a271945a
victor loginov <tkoe88@zoho.com>
2024-10-31 22:19:22 +0100

    difficulty sets timer, lives and score

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ export default class User {
      choice:  string;
   isCorrect: boolean;
        item: boolean;
+    penalty:  number;
       score:  number;
       total:  number;
      solved:  number;
@@ -9,23 +10,31 @@ export default class User {
       level:  number;
       lives:  number;
 
- constructor(level: number) {
-   this.choice    = '';
-   this.isCorrect = false;
-   this.item      = false;
-   this.score     = 0;
-   this.total     = 0;
-   this.solved    = 0;
-   this.streak    = 0;
-   this.level     = level;
-   this.lives     = 20 - (5 * level); // einfash: 15, mittel: 10, schwer: 5
- }
+  constructor(level: number) {
+    this.choice    = '';
+    this.isCorrect = false;
+    this.item      = false;
+    this.penalty   = 1;
+    this.score     = 0;
+    this.total     = 0;
+    this.solved    = 0;
+    this.streak    = 0;
+    this.level     = level;
+    this.lives     = Math.floor(10 / level); // einfash: 10, mittel: 5, schwer: 3
+  }
 
- static setDifficulty(level: number) {
-  localStorage.setItem('difficulty', JSON.stringify({ level }));
- }
+  static setDifficulty(level: number) {
+    localStorage.setItem('difficulty', JSON.stringify({ level }));
+  }
 
- static getDifficulty() {
-  return JSON.parse(localStorage['difficulty'] || '{ "level": 1 }').level;
- }
+  static getDifficulty() {
+    return JSON.parse(localStorage['difficulty'] || '{ "level": 1 }').level;
+  }
+
+  static getScore(level: number, streak: number, penalty: number, timeRemaining: number) {
+    const    divider = 500 / (level + (level - 1) * (level - 2)); // 1: 500, 2: 250, 3: 100
+    const  baseScore = timeRemaining / divider; // 1: 30, 2: 40, 3: 50 max
+    const multiplier = Math.min(streak * 0.1 + 0.9, 2) // 1.0 - 2.0
+    return Math.round(baseScore * multiplier / penalty);
+  }
 }
