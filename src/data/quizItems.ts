098ffea5ebef31e429da862ded89d1c4fba46896
victor loginov <tkoe88@zoho.com>
2024-11-04 18:13:29 +0100

    cleaned up reflex/stative fns. corrected dative plural fn

--- src/data/quizItems.ts
+++ src/data/quizItems.ts
@@ -107,18 +107,17 @@ const objects = [
 ];
 
 type Verb = {
-     body: string;
-     prep: string;
-     case: string;
-     end?: string;
-  reflex?: boolean;
- stative?: boolean;
-    //  mod?: { e?: string, st?: string, t?: string, en?: string };
-     mod?: object;
+      body: string;
+      prep: string;
+      case: string;
+      end?: string;
+  stative?: object;
+   reflex?: object;
+      mod?: object;
 }
 
-const     reflex = true;
-const    stative = true;
+const     reflex = { e: 'mich', st: 'dich', t: 'sich', en: 'sich', Wir: 'uns', Ihr: 'euch' };
+const    stative = { Ich: 'bin', Du: 'bist', Ihr: 'seid', t: 'ist', en: 'sind'   };
 const        mod =            { st: 'est',   t: 'et',                Ihr: 'et'   };
 const   zweifeln = { e: 'le',   st: 'elst',  t: 'elt',  en: 'eln'                };
 const  verlassen = { e: 'asse', st: 'ässt',  t: 'ässt', en: 'assen', Ihr: 'asst' };
@@ -203,23 +202,6 @@ const verbs = [
   { body: 'zuständig',         prep: 'für',   case: 'akk', stative }
 ];
 
-const reflexes = {
-  e:   'mich',
-  st:  'dich',
-  t:   'sich',
-  en:  'sich',
-  Wir: 'uns',
-  Ihr: 'euch',
-}
-
-const states = {
-  Ich: 'bin',
-   Du: 'bist',
-  Ihr: 'seid',
-    t: 'ist',
-   en: 'sind',
-}
-
 const rand = (max: number) => Math.floor(Math.random() * max);
 
 const getAdjective = (adj: string, CASE: string, gend: string) =>
@@ -240,13 +222,15 @@ const getPossesive = (possesive: string, CASE: string, gend: string) => {
 }
 
 const getVerb = (verb: Verb, subject: Subject) => {
-  const modKey = subject.body === 'Ihr' ? 'Ihr' : subject.end;
-  const modEnd = verb.mod?.[modKey as keyof typeof verb.mod];
-  const   body = verb.stative ? verb.body : verb.body + (modEnd ? modEnd : subject.end);
-  const refKey = ['Wir', 'Ihr'].includes(subject.body) ? subject.body : subject.end;
-  const reflex = verb.reflex ? reflexes[refKey as keyof typeof reflexes] : '';
+  const   modKey = subject.body === 'Ihr' ? 'Ihr' : subject.end;
+  const   modEnd = verb.mod?.[modKey as keyof typeof verb.mod];
+  const     body = verb.stative ? verb.body : verb.body + (modEnd ? modEnd : subject.end);
+  const   refKey = ['Wir', 'Ihr'].includes(subject.body) ? subject.body : subject.end;
+  const   reflex = verb.reflex?.[refKey as keyof typeof verb.reflex];
+  const stateKey = ['Ich', 'Du', 'Ihr'].includes(subject.body) ? subject.body : subject.end;
+  const  stative = verb.stative?.[stateKey as keyof typeof verb.stative];
 
-  return { ...verb, body, reflex };
+  return { ...verb, body, reflex, stative };
 }
 
 const question = () => {
@@ -256,17 +240,16 @@ const question = () => {
   const       verb =      getVerb(      verbs[rand(    verbs.length)],  subject);
   const  adjective = getAdjective( adjectives[rand(adjectives.length)],  verb.case, object.gend);
   const  possesive = getPossesive(possessives[rand(possessives.length)], verb.case, object.gend);
-  const   stateKey = ['Ich', 'Du', 'Ihr'].includes(subject.body) ? subject.body : subject.end;
-  const      state = verb.stative ? states[stateKey as keyof typeof states] : '';
-  const  pluralDat = verb.case === 'dat' ? (/n$/.test(object.body) ? '' : 'n') : '';
+  const pluralDat =
+    verb.case === 'dat' && object.gend === 'p' && !object.body.endsWith('n') ? 'n' : '';
 
   const body = [
     subject.body,
-    state,
-    state ? adverb : '',
+    verb.stative,
+    verb.stative ? adverb : '',
     verb.body,
     verb.reflex,
-    !state ? adverb : '',
+    !verb.stative ? adverb : '',
     verb.prep,
     possesive,
     adjective,
@@ -281,4 +264,4 @@ for (let i = 0; i < 7; i++) {
   console.log(question());
 }
 
-export { subjects, objects, verbs, reflexes, states, adverbs, adjectives, possessives };
+export { subjects, objects, verbs, adverbs, adjectives, possessives };
--- src/models/Question.ts
+++ src/models/Question.ts
@@ -1,19 +1,17 @@
-import { subjects, objects, verbs, reflexes, states, adverbs, adjectives, possessives } from '@/data/quizItems';
+import { subjects, objects, verbs, adverbs, adjectives, possessives } from '@/data/quizItems';
 import rand from '@/util/rand';
 
-type Subject = { body: string, end: string };
+type Subject = { body: string; end: string };
 
 type Verb = {
-     body: string;
-     prep: string;
-     case: string;
-     end?: string;
-  reflex?: boolean;
- stative?: boolean;
-     mod?: object;
-    //  mod?: { e?: string, st?: string, t?: string, en?: string };
-}
-
+      body: string;
+      prep: string;
+      case: string;
+      end?: string;
+  stative?: object;
+   reflex?: object;
+      mod?: object;
+};
 const getAdjective = (adj: string, CASE: string, gend: string) =>
   adj + (CASE === 'dat' || ['m', 'p'].includes(gend) ? 'en' : gend === 'n' ? 'es' : 'e');
 
@@ -32,13 +30,15 @@ const getPossesive = (possesive: string, CASE: string, gend: string) => {
 }
 
 const getVerb = (verb: Verb, subject: Subject) => {
-  const modKey = subject.body === 'Ihr' ? 'Ihr' : subject.end;
-  const modEnd = verb.mod?.[modKey as keyof typeof verb.mod];
-  const   body = verb.stative ? verb.body : verb.body + (modEnd ? modEnd : subject.end);
-  const refKey = ['Wir', 'Ihr'].includes(subject.body) ? subject.body : subject.end;
-  const reflex = verb.reflex ? reflexes[refKey as keyof typeof reflexes] : '';
+  const   modKey = subject.body === 'Ihr' ? 'Ihr' : subject.end;
+  const   modEnd = verb.mod?.[modKey as keyof typeof verb.mod];
+  const     body = verb.stative ? verb.body : verb.body + (modEnd ? modEnd : subject.end);
+  const   refKey = ['Wir', 'Ihr'].includes(subject.body) ? subject.body : subject.end;
+  const   reflex = verb.reflex?.[refKey as keyof typeof verb.reflex];
+  const stateKey = ['Ich', 'Du', 'Ihr'].includes(subject.body) ? subject.body : subject.end;
+  const  stative = verb.stative?.[stateKey as keyof typeof verb.stative];
 
-  return { ...verb, body, reflex };
+  return { ...verb, body, reflex, stative };
 }
 
 export default class Question {
@@ -51,29 +51,28 @@ export default class Question {
   }
 
   static random() {
-    const    subject = subjects[rand(subjects.length)];
-    const     object =  objects[rand( objects.length)];
-    const     adverb =  adverbs[rand( adverbs.length)];
-    const       verb =      getVerb(      verbs[rand(    verbs.length)],  subject);
-    const  adjective = getAdjective( adjectives[rand(adjectives.length)],  verb.case, object.gend);
-    const  possesive = getPossesive(possessives[rand(possessives.length)], verb.case, object.gend);
-    const   stateKey = ['Ich', 'Du', 'Ihr'].includes(subject.body) ? subject.body : subject.end;
-    const      state = verb.stative ? states[stateKey as keyof typeof states] : '';
-    const  pluralDat = verb.case === 'dat' ? (/n$/.test(object.body) ? '' : 'n') : '';
+    const   subject = subjects[rand(subjects.length)];
+    const    object =  objects[rand( objects.length)];
+    const    adverb =  adverbs[rand( adverbs.length)];
+    const      verb = getVerb(verbs[rand(verbs.length)], subject);
+    const adjective = getAdjective( adjectives[rand(adjectives.length)], verb.case, object.gend);
+    const possesive = getPossesive(possessives[rand(possessives.length)], verb.case, object.gend);
+    const pluralDat =
+      verb.case === 'dat' && object.gend === 'p' && !object.body.endsWith('n') ? 'n' : '';
 
     const body = [
       subject.body,
-      state,
-      state ? adverb : '',
+      verb.stative,
+      verb.stative ? adverb : '',
       verb.body,
       verb.reflex,
-      !state ? adverb : '',
+      !verb.stative ? adverb : '',
       '___',
       possesive,
       adjective,
       object.body + pluralDat,
-      verb.end ?? ''
-    ].filter(part => part).join(' ');
+      verb.end ?? '',
+    ].filter((part) => part).join(' ');
 
     return { body, ans: verb.prep };
   };
