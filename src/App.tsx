278170691a7da5a78ad62d26a93e24f095424e43
victor loginov <tkoe88@zoho.com>
2025-10-26 01:48:48 +0200

    fontawesome icons imports reduced

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
+import './icons';
 import { useRoutes } from 'react-router-dom';
-import RootLayout from './pages/Root';
-import GamePage from './pages/Game';
-import VocabPage from './pages/Vocab';
-import SettingsPage from './pages/Settings';
+import RootLayout    from './pages/Root';
+import GamePage      from './pages/Game';
+import VocabPage     from './pages/Vocab';
+import SettingsPage  from './pages/Settings';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons'; // import brand icons
-import { fas } from '@fortawesome/free-solid-svg-icons'; // import solid icons
-import { far } from '@fortawesome/free-regular-svg-icons'; // import regular icons
-
-library.add(fab, fas, far);
+const routes = [
+  { path: '/',         element: <GamePage />     },
+  { path: '/vocab',    element: <VocabPage />    },
+  { path: '/settings', element: <SettingsPage /> }
+];
 
 export default function App() {
-  const element = useRoutes(
-    [
-      { path: '/',         element: <GamePage /> },
-      { path: '/vocab',    element: <VocabPage /> },
-      { path: '/settings', element: <SettingsPage /> }
-    ]
-  );
-
-  if (!element) return null;
-
-  return <RootLayout>{element}</RootLayout>;
+  const element = useRoutes(routes);
+  return element ? <RootLayout children={element} /> : null;
 }
