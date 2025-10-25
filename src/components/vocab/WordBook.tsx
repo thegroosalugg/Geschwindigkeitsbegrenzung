b0afa87d47933586718330ad6b8435e935deb4c3
victor loginov <tkoe88@zoho.com>
2025-10-25 17:42:47 +0200

    created reusable header component

--- src/components/vocab/WordBook.tsx
+++ src/components/vocab/WordBook.tsx
@@ -1,15 +1,17 @@
-import { motion } from 'framer-motion';
-import { translations } from '@/data/translations';
-import WordEntry from './WordEntry';
-import css from './WordBook.module.css';
+import { motion } from "framer-motion";
+import { translations } from "@/data/translations";
+import WordEntry from "./WordEntry";
+import Header from "../ui/Header";
+import css from "./WordBook.module.css";
 
 export default function WordBook() {
   return (
     <>
-      <motion.h1 className={css['wordbook-header']} animate={{ opacity: [0, 1], transition: { duration: 1, ease: 'easeIn' } }}>
-        Verben
-      </motion.h1>
-      <motion.ul className={css['wordbook-list']} animate={{ opacity: [0, 1], transition: { duration: 1, ease: 'easeIn' } }}>
+      <Header>Verben</Header>
+      <motion.ul
+        className={css["wordbook"]}
+          animate={{ opacity: [0, 1], transition: { duration: 1, ease: "easeIn" } }}
+      >
         {translations.map((word) => (
           <WordEntry key={word.body} {...word} />
         ))}
