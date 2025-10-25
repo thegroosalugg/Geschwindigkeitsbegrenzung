f4180c1cdcba331de4185c75342d8aa0a46eef1d
victor loginov <tkoe88@zoho.com>
2025-10-25 20:20:04 +0200

    updated start screen

--- src/components/quiz/StartScreen.tsx
+++ src/components/quiz/StartScreen.tsx
@@ -1,45 +1,37 @@
 import { motion } from 'framer-motion';
 import Timer from '@/models/Timer';
 import useDebounce from '@/hooks/useDebounce';
+import Header from '../ui/Header';
+import User from '@/models/User';
 import css from './StartScreen.module.css';
+
+const backgrounds = ['easy', 'medium', 'hard'];
 
 export default function StartScreen({ timer }: { timer: Timer }) {
   const { isDebouncing, throttleFn } = useDebounce();
-  const mouseEvent = (scale: number) =>
-    ({ scale, color: '#FFFFFF', textShadow: '2px 2px 4px #4B0082', borderColor: '#e6e6e6' });
+  const level = User.getDifficulty();
 
   return (
-    <section className={css['start-screen']}>
-      <motion.h1
-        animate={{ opacity: [0, 1], transition: { duration: 1, ease: 'easeIn' } }}
-           exit={{  scaleY:      0, transition: { duration: 0.5 } }}
-      >
-        Geschwindigkeitsbegrenzung
-      </motion.h1>
+    <>
+      <Header>Geschwindigkeitsbegrenzung</Header>
       <motion.img
-            src='/skeletons.png'
-            alt='skeletons playing instruments'
-        animate={{ opacity: [0, 1], transition: { duration: 1, ease: 'easeIn' } }}
-           exit={{ opacity: 0 }}
+        className={css['cover-img']}
+              src='/skeletons.png'
+              alt='skeletons playing instruments'
+          animate={{ opacity: [0, 1], transition: { duration: 1, ease: 'easeIn' } }}
+             exit={{ opacity: 0 }}
       />
       <motion.button
+         className={css['play-btn']}
           disabled={isDebouncing}
            onClick={() => throttleFn(timer.start, 700)}
-        whileHover={mouseEvent(1.1)}
-          whileTap={mouseEvent(1.2)}
+             style={{ background: `var(--${backgrounds[level - 1]})`}}
+        whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 1.1  }}
               exit={{ scale: 0, opacity: 0, transition: { duration: 0.5 } }}
-           animate={{
-             opacity: [0, 1],
-               scale: [1.2, 1],
-          background: ['#898121', '#859F3D', '#C4E1F6', '#789DBC', '#257180'],
-          transition: {
-            background: { duration: 8, repeat: Infinity, repeatType: 'mirror' },
-              duration: 0.7,
-          },
-        }}
       >
-        Präpositionen
+        Spiel
       </motion.button>
-    </section>
+    </>
   );
 }
